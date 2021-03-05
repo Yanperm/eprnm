4ec9db00cc9560adcc175f7ca9fd3223ecbaee68
Mr.Pratchayanan <Yanperm@users.noreply.github.com>
2021-03-05 12:09:29 +0700

    Update config.php

--- application/config/config.php
+++ application/config/config.php
@@ -24,7 +24,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 |
 */
 //$config['base_url'] = '';
-$config['base_url'] = 'http://localhost/eprnm/';
+$config['base_url'] = 'https://demo.nmepr.com/';
 
 /*
 |--------------------------------------------------------------------------
@@ -381,8 +381,8 @@ $config['encryption_key'] = '';
 $config['sess_driver'] = 'files';
 $config['sess_cookie_name'] = 'ci_session';
 $config['sess_expiration'] = 7200;
-$config['sess_save_path'] =  APPPATH. 'cache/';
-$config['sess_match_ip'] = true;
+$config['sess_save_path'] = NULL;
+$config['sess_match_ip'] = FALSE;
 $config['sess_time_to_update'] = 300;
 $config['sess_regenerate_destroy'] = FALSE;
 
