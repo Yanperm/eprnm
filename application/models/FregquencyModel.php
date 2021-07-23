d765cac4c20cff6af567c902f924617e85e33fa4
supharatnop <supharat_n@kkumail.com>
2021-07-23 13:53:23 +0700

    Add : CRUD tbFregquency

---
+++ application/models/FregquencyModel.php
@@ -0,0 +1,47 @@
+<?php 
+defined('BASEPATH') or exit('No direct script access allowed');
+
+class FregquencyModel extends CI_Model{
+
+    public function getAllData(){
+        $query = $this->db->get('tbfregquency');
+
+        if($query->num_rows()>0){
+            return $query->result();
+        }else{
+            return array();
+        }
+
+    }
+
+    public function getDataByID($id){
+        $query = $this->db->query('SELECT * FROM tbfregquency where id = "'.$id.'"');
+
+        if($query->num_rows()>0){
+            return $query->row();
+        }else{
+            return array();
+        }
+    }
+
+    public function insert($data){
+        $this->db->insert('tbfregquency',$data);
+        return true;
+    }
+
+    public function update($data,$id){
+        $this->db->where('id',$id);
+        $this->db->update('tbfregquency',$data);
+        return true;
+    }
+
+    public function delete($id){
+        $this->db->where('id',$id);
+        $this->db->delete('tbfregquency');
+        return true;
+    }
+}
+
+
+
+?>
