02aa3c2e9d38fcb19ef48d0f7f47de8948b529ad
wanalbino <wanwancsyy@gmail.com>
2021-07-30 08:52:27 +0700

    Add : Reciept paper

--- application/controllers/RecordCost.php
+++ application/controllers/RecordCost.php
@@ -53,4 +53,123 @@ class RecordCost extends CI_Controller {
         $this->load->view('record_cost/index', $data);
         $this->load->view('template/footer');
     }
+
+    public function receipt(){
+        $member = $this->MemberModel->getDataById($_GET['id']);
+        $bookingId = $_GET['booking_id'];
+        $clinic = $this->ClinicModel->detailById($this->session->userdata('id'));
+        $diagnose = $this->PatientHistoryModel->getDataById($bookingId);
+        $medical = $this->RecordMedicalModel->getDataByBookingId($bookingId);
+        $lab = $this->RecordLabModel->getDataByBookingId($bookingId);
+        $procedure = $this->RecordProcedureModel->getDataByBookingId($bookingId);
+        $certificateJob = $this->PatientJobModel->getDataByBookingId($bookingId);
+        $certificateSick = $this->PatientSickModel->getDataByBookingId($bookingId);
+
+       
+            $this->load->library('Pdf');
+            $this->load->library('parser');
+        
+            $pageLayout = array(100, 80);
+            $pdf = new Pdf();
+            
+            $font = 'thsarabun';
+            $pdf->font = $font;
+            $pdf->SetCreator("");
+            $pdf->SetAuthor("");
+            $pdf->SetTitle("ใบเสร็จ");
+            $pdf->SetSubject("ใบเสร็จ");
+            $pdf->setPrintHeader(false);
+            $pdf->setPrintFooter(false);
+            $pdf->SetAutoPageBreak(TRUE, 0);
+            $pdf->SetMargins(5, 5, 5);
+            $pdf->SetHeaderMargin(0);
+            $pdf->SetTopMargin(5);
+            $pdf->SetFooterMargin(0);
+            $pdf->SetFont($font, '', 10);
+            // Add a page
+            $width = $pdf->pixelsToUnits(283); 
+            $height = $pdf->pixelsToUnits(212);
+            $resolution= array(150, 80);
+            $pdf->AddPage('P', $resolution, true, 'UTF-8', false);
+        
+           // $html = '<img src="'.base_url().'assets/img/nutmor_logo02.png" width="20px" height="15px">';
+            $html = '<span style="text-align:center"><b>'.$clinic->CLINICNAME."</b></span>";
+            $html .= "<br>".'<span style="text-align:center">'.$clinic->ADDRESS.'</span>';
+            $html .= "<br>".'<span style="text-align:right">Tel : '.$clinic->PHONE.'</span>';
+            $html .= "<br>".'<span style="text-align:left">ต้นฉบับใบเสร็จรับเงิน</span>';
+            $html .= "<br>".'<span style="text-align:left">วันที่ '.date('d/m/Y H:i').'</span>';
+            $html .= "<hr>";
+            $html .= "<br>".'<span style="text-align:left">ลูกค้า : '.$member->CUSTOMERNAME.'</span>';
+            $html .= "<hr>";
+            $i = 0;
+            $total = 0;
+            $html .= '<table style="width:92%">';
+            if(!empty($diagnose)){
+                $i ++;
+                $html .= '<tr ><td style="width:60%"><b>'.$i.".Diagnose</b></td><td></td></tr>";
+                foreach($diagnose as $item){
+                    $total += $item->PH5;
+                    $html .= '<tr><td>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;'.$item->PH3.'</td><td style="text-align:right">'.number_format($item->PH5, 2)."</td></tr>";
+                }  
+            }
+            if(!empty($medical)){
+                $i ++;
+                $html .= '<tr><td><b>'.$i.".Medicine</b></td><td></td></tr>";
+                foreach($medical as $item){
+                    $total += $item->PH8;
+                    $html .= '<tr><td>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;'.$item->PH1.'</td><td style="text-align:right">'.number_format($item->PH8, 2)."</td></tr>";
+                }  
+            }
+            if(!empty($lab)){
+                $i ++;
+                $html .= '<tr><td><b>'.$i.".Laboratory</b></td><td></td></tr>";
+                foreach($lab as $item){
+                    $total += $item->PH4;
+                    $html .= '<tr><td>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;'.$item->PH1.'</td><td style="text-align:right">'.number_format($item->PH4, 2)."</td></tr>";
+                }  
+            }
+            if(!empty($procedure)){
+                $i ++;
+                $html .= '<tr><td><b>'.$i.".Procedure</b></td><td></td></tr>";
+                foreach($procedure as $item){
+                    $total += $item->PH3;
+                    $html .= '<tr><td>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;'.$item->PH2.'</td><td style="text-align:right">'.number_format($item->PH3, 2)."</td></tr>";
+                }  
+            }
+            if(!empty($certificateJob) || !empty($certificateSick)){
+                $i ++;
+                $html .= '<tr><td><b>'.$i.".Certificate</b></td><td></td></tr>";
+                if(!empty($certificateJob)){
+                    foreach($certificateJob as $item){
+                        $total += $item->Price;
+                        $html .= '<tr><td>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;ค่าใบรับรองแพทย์
+                        สำหรับสมัครงาน</td><td style="text-align:right">'.number_format($item->Price, 2)."</td></tr>";
+                    }  
+                }
+                
+                if(!empty($certificateSick)){
+                    foreach($certificateJob as $item){
+                        $total += $item->Price;
+                        $html .= '<tr><td>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;ค่าใบรับรองแพทย์สำหรับลาป่วย</td><td style="text-align:right">'.number_format($item->Price, 2)."</td></tr>";
+                    }  
+                }   
+            }
+            $html .= '<tr><td><b>Sub Total</b></td><td style="text-align:right">'.number_format($total, 2).'</td></tr>';
+            $html .= '<tr><td><b>Discount</b></td><td style="text-align:right">'.number_format(0, 2).'</td></tr>';
+            $html .= '<tr><td><b>Net Total</b></td><td style="text-align:right">'.number_format($total, 2).'</td></tr>';
+            $html .= "<hr>";
+            $html .= '<tr><td><b>CASH</b></td><td style="text-align:right">'.number_format($total, 2).'</td></tr>';
+            $html .= "<hr>";
+            $html .= "</table><br>";
+            $html .= '<br><br><span style="text-align:center;">*โปรดตรวจสอบรายการทุกครั้งเพื่อความถูกต้อง*</span>';
+           
+           
+            
+            $pdf->writeHTMLCell(0, 0, '', '', $html, 0, 1, 0, true, '', true);
+          
+            $pdf->lastPage();
+            $pdf->Output('Student_list.pdf', 'I');
+        
+
+    }
 }
--- application/views/record_cost/index.php
+++ application/views/record_cost/index.php
@@ -6,15 +6,20 @@
                     <div class="card">
                         <div class="tab-container">
                             <ul class="nav nav-tabs" role="tablist">
-                                <li class="nav-item"><a class="nav-link " href="<?php echo base_url('recordHistory');?>?id=<?php echo $member->MEMBERIDCARD;?>&booking_id=<?php echo $bookingId;?>"><span
+                                <li class="nav-item"><a class="nav-link "
+                                        href="<?php echo base_url('recordHistory');?>?id=<?php echo $member->MEMBERIDCARD;?>&booking_id=<?php echo $bookingId;?>"><span
                                             class="icon mdi mdi-assignment-account"></span>รายการตรวจวินิจฉัย</a></li>
-                                <li class="nav-item"><a class="nav-link " href="<?php echo base_url('recordInformation');?>?id=<?php echo $member->MEMBERIDCARD;?>&booking_id=<?php echo $bookingId;?>"><span
+                                <li class="nav-item"><a class="nav-link "
+                                        href="<?php echo base_url('recordInformation');?>?id=<?php echo $member->MEMBERIDCARD;?>&booking_id=<?php echo $bookingId;?>"><span
                                             class="icon mdi mdi-male"></span>ข้อมูลผู้ป่วย</a></li>
-                                <li class="nav-item"><a class="nav-link " href="<?php echo base_url('recordPatient');?>?id=<?php echo $member->MEMBERIDCARD;?>&booking_id=<?php echo $bookingId;?>"><span
+                                <li class="nav-item"><a class="nav-link "
+                                        href="<?php echo base_url('recordPatient');?>?id=<?php echo $member->MEMBERIDCARD;?>&booking_id=<?php echo $bookingId;?>"><span
                                             class="icon mdi mdi-local-hospital"></span>การรักษา</a></li>
-                                <li class="nav-item"><a class="nav-link" href="<?php echo base_url('recordDrug');?>?id=<?php echo $member->MEMBERIDCARD;?>&booking_id=<?php echo $bookingId;?>"><span
+                                <li class="nav-item"><a class="nav-link"
+                                        href="<?php echo base_url('recordDrug');?>?id=<?php echo $member->MEMBERIDCARD;?>&booking_id=<?php echo $bookingId;?>"><span
                                             class="icon mdi mdi-local-pharmacy"></span>ห้องยา</a></li>
-                                <li class="nav-item"><a class="nav-link active" href="<?php echo base_url('recordCost');?>?id=<?php echo $member->MEMBERIDCARD;?>&booking_id=<?php echo $bookingId;?>"><span
+                                <li class="nav-item"><a class="nav-link active"
+                                        href="<?php echo base_url('recordCost');?>?id=<?php echo $member->MEMBERIDCARD;?>&booking_id=<?php echo $bookingId;?>"><span
                                             class="icon mdi mdi-card"></span>ค่าใช้จ่าย</a></li>
                             </ul>
                             <div class="tab-content">
@@ -26,22 +31,30 @@
                                                     <div class="invoice">
                                                         <div class="row invoice-header">
                                                             <div class="col-sm-7">
-                                                                <div class="invoice-logo" style="background-repeat: no-repeat;background-size: 54px 51px;background-image: url(assets/img/nutmor_logo02.png);"></div>
+                                                                <div class="invoice-logo"
+                                                                    style="background-repeat: no-repeat;background-size: 54px 51px;background-image: url(assets/img/nutmor_logo02.png);">
+                                                                </div>
                                                             </div>
-                                                            <div class="col-sm-5 invoice-order"><span class="invoice-id">Invoice
-                                                                    #<?php echo $bookingId;?></span><span class="incoice-date"><?php echo date("Y-m-d");?></span>
+                                                            <div class="col-sm-5 invoice-order"><span
+                                                                    class="invoice-id">Invoice
+                                                                    #<?php echo $bookingId;?></span><span
+                                                                    class="incoice-date"><?php echo date("Y-m-d");?></span>
                                                             </div>
                                                         </div>
                                                         <div class="row invoice-data">
                                                             <div class="col-sm-5 invoice-person">
-                                                                <span class="name"><b><?php echo $clinic->CLINICNAME;?></b></span>
+                                                                <span
+                                                                    class="name"><b><?php echo $clinic->CLINICNAME;?></b></span>
                                                                 <span><?php echo $clinic->DOCTORNAME;?></span>
                                                                 <span><?php echo $clinic->PROFICIENT;?></span>
                                                                 <span><?php echo $clinic->ADDRESS;?></span>
-                                                                <span><?php echo $clinic->PHONE;?></span></div>
-                                                            <div class="col-sm-2 invoice-payment-direction"><i class="icon mdi mdi-chevron-right"></i></div>
+                                                                <span><?php echo $clinic->PHONE;?></span>
+                                                            </div>
+                                                            <div class="col-sm-2 invoice-payment-direction"><i
+                                                                    class="icon mdi mdi-chevron-right"></i></div>
                                                             <div class="col-sm-5 invoice-person">
-                                                                <span class="name"><?php echo $member->CUSTOMERNAME;?></span>
+                                                                <span
+                                                                    class="name"><?php echo $member->CUSTOMERNAME;?></span>
                                                                 <span><?php echo $member->PHONE;?></span>
                                                                 <span><?php echo $member->LINEID;?></span>
                                                                 <span><?php echo $member->EMAIL;?></span>
@@ -52,7 +65,8 @@
                                                                         <tr>
                                                                             <th style="width:80%">รายการ</th>
 
-                                                                            <th class="amount" style="width:20%">ราคา</th>
+                                                                            <th class="amount" style="width:20%">ราคา
+                                                                            </th>
                                                                         </tr>
                                                                         <?php 
                                                                         $total = 0;
@@ -66,7 +80,8 @@
                                                                                             $total += $item->PH5;
                                                                                             ?>
                                                                                             <li class="mt-2">
-                                                                                                <?php echo $item->PH3;?><span style="float: right;"><?php echo number_format($item->PH5, 2);?></span>
+                                                                                                <?php echo $item->PH3;?><span
+                                                                                                    style="float: right;"><?php echo number_format($item->PH5, 2);?></span>
                                                                                             </li>
                                                                                             <?php endforeach;?>
                                                                                         </ul>
@@ -86,7 +101,8 @@
                                                                                 $total += $item->PH8;
                                                                             ?>
                                                                                             <li class="mt-2">
-                                                                                                <?php echo $item->PH1;?><span style="float: right;"><?php echo number_format($item->PH8, 2);?></span>
+                                                                                                <?php echo $item->PH1;?><span
+                                                                                                    style="float: right;"><?php echo number_format($item->PH8, 2);?></span>
                                                                                             </li>
                                                                                             <?php endforeach;?>
                                                                                         </ul>
@@ -105,7 +121,8 @@
                                                                                 $total += $item->PH4;
                                                                             ?>
                                                                                             <li class="mt-2">
-                                                                                                <?php echo $item->PH1;?><span style="float: right;"><?php echo number_format($item->PH4, 2);?></span>
+                                                                                                <?php echo $item->PH1;?><span
+                                                                                                    style="float: right;"><?php echo number_format($item->PH4, 2);?></span>
                                                                                             </li>
                                                                                             <?php endforeach;?>
                                                                                         </ul>
@@ -124,7 +141,8 @@
                                                                                 $total += $item->PH3;
                                                                             ?>
                                                                                             <li class="mt-2">
-                                                                                                <?php echo $item->PH2;?><span style="float: right;"><?php echo number_format($item->PH3, 2);?></span>
+                                                                                                <?php echo $item->PH2;?><span
+                                                                                                    style="float: right;"><?php echo number_format($item->PH3, 2);?></span>
                                                                                             </li>
                                                                                             <?php endforeach;?>
                                                                                         </ul>
@@ -144,7 +162,9 @@
                                                                                 $total += $item->Price;
                                                                             ?>
                                                                                             <li class="mt-2">
-                                                                                                ค่าใบรับรองแพทย์ สำหรับสมัครงาน<span style="float: right;"><?php echo number_format($item->Price, 2);?></span>
+                                                                                                ค่าใบรับรองแพทย์
+                                                                                                สำหรับสมัครงาน<span
+                                                                                                    style="float: right;"><?php echo number_format($item->Price, 2);?></span>
                                                                                             </li>
                                                                                             <?php endforeach;?>
                                                                                             <?php endif;?>
@@ -153,7 +173,9 @@
                                                                                 $total += $item->Price;
                                                                             ?>
                                                                                             <li class="mt-2">
-                                                                                                ค่าใบรับรองแพทย์ สำหรับลาป่วย<span style="float: right;"><?php echo number_format($item->Price, 2);?></span>
+                                                                                                ค่าใบรับรองแพทย์
+                                                                                                สำหรับลาป่วย<span
+                                                                                                    style="float: right;"><?php echo number_format($item->Price, 2);?></span>
                                                                                             </li>
                                                                                             <?php endforeach;?>
                                                                                             <?php endif;?>
@@ -179,9 +201,13 @@
                                                             </div>
 
                                                             <div class="row invoice-company-info">
-                                                                <div class="col-md-6 col-lg-2 logo"><img style="width: 50px;" src="assets/img/nutmor_logo02.png" alt="Logo-symbol">
+                                                                <div class="col-md-6 col-lg-2 logo"><img
+                                                                        style="width: 50px;"
+                                                                        src="assets/img/nutmor_logo02.png"
+                                                                        alt="Logo-symbol">
                                                                 </div>
-                                                                <div class="col-md-6 col-lg-4 summary"><span class="title"><?php echo $clinic->CLINICNAME;?></span>
+                                                                <div class="col-md-6 col-lg-4 summary"><span
+                                                                        class="title"><?php echo $clinic->CLINICNAME;?></span>
                                                                     <p>
                                                                         <?php echo $clinic->DOCTORNAME;?>
                                                                     </p>
@@ -204,9 +230,14 @@
                                                             </div>
                                                             <div class="row invoice-footer col-lg-12">
                                                                 <div class="col-lg-12">
-                                                                    <button class="btn btn-lg btn-space btn-secondary">PDF</button>
-                                                                    <button class="btn btn-lg btn-space btn-secondary">พิมพ์</button>
-                                                                    <button class="btn btn-lg btn-space btn-primary">ชำระเงิน</button>
+                                                                    <button
+                                                                        class="btn btn-lg btn-space btn-secondary">PDF</button>
+                                                                    <a href="<?php echo base_url('recordCost/receipt');?>?id=<?php echo $member->MEMBERIDCARD;?>&booking_id=<?php echo $bookingId;?>"
+                                                                        class="btn btn-lg btn-space btn-secondary"
+                                                                        target="_blank">พิมพ์
+                                                                    </a>
+                                                                    <button
+                                                                        class="btn btn-lg btn-space btn-primary">ชำระเงิน</button>
                                                                 </div>
                                                             </div>
                                                         </div>
