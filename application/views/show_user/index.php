fcabb211dff8f274799e650b00e87b856473016c
supharatnop <supharat_n@kkumail.com>
2021-08-24 13:59:21 +0700

    commit : config

--- application/views/show_user/index.php
+++ application/views/show_user/index.php
@@ -10,49 +10,65 @@
 <div class="row" id="vue-root">
     <div class="col-md-12">
         <div class="card card-table">
-            <div class="row table-filters-container">
+        <div style="background-color:#CCCCFF;">
+                <button type="submit" class="btn  btn-lg" @click="" >
+                <i class="icon mdi mdi-account"></i> เพิ่มผู้ใช้งาน</button>
+            </div>
+            <div class="col-12 ">
+                <h3>การจัดการผู้ใช้งาน</h3>
+                <label>เพิ่มและแก้ไขข้อมูลเกี่ยวกับผู้ใช้งานระบบคลินิก</label>
+            </div><br>
+            <!-- <div class="row table-filters-container">
                 <div class="col-12 col-lg-12 col-xl-6">
                     <div class="row">
                         <div class="col-12 col-lg-6 table-filters pb-0 pb-xl-4"><span class="table-filter-title">ค้นหาผู้ใช้งานระบบ</span>
                             <div class="filter-container">
                                 <form>
-                                    <!-- <label class="control-label">โปรดพิมพ์คำที่ต้องการค้นหา</label> -->
                                     <at-input v-model="search" size="large" placeholder="ค้นหา"></at-input>
                                 </form>
                             </div>
                         </div>
                     </div>
                 </div> 
-            </div>
+            </div> -->
             <div class="card-body">
                 <div class="noSwipe">
 
                     <div>
                         <vs-table :sst="true" @sort="handleSort" v-model="selected" :total="totalItems" :max-items="perPage" :data="recordData">
                             <template slot="thead">
-                            <vs-th sort-key="Nameprefix">
+                            <!-- <vs-th sort-key="Nameprefix">
                                     คำนำหน้าชื่อ
-                                </vs-th>  
-                            <vs-th sort-key="UserName">
+                                </vs-th>   -->
+                                <vs-th sort-key="UserName">
                                     ชื่อ
+                                </vs-th>
+                                <vs-th sort-key="UserName">
+                                    ประเภท
                                 </vs-th>
                                 <vs-th sort-key="Email">
                                     E-mail
                                 </vs-th>
-                                <vs-th sort-key="Address">
+                                <!-- <vs-th sort-key="Address">
                                     ที่อยู่
-                                </vs-th>
+                                </vs-th> -->
                                 <vs-th sort-key="Phone">
                                     เบอร์โทรศัพท์
                                 </vs-th>
-                                <vs-th sort-key="License">
+                                <!-- <vs-th sort-key="License">
                                     ใบอนุญาต
+                                </vs-th> -->
+                                <vs-th class ="centerx">
+                                    จัดการ
                                 </vs-th>
                             </template>
                             <template slot-scope="{data}">
                                 <vs-tr :data="tr" :key="indextr" v-for="(tr, indextr) in data" >
-                                    <vs-td :data="data[indextr].Nameprefix">
+                                    <!-- <vs-td :data="data[indextr].Nameprefix">
                                         {{data[indextr].Nameprefix}}
+                                    </vs-td> -->
+                                    <vs-td :data="data[indextr].UserName">
+                                        {{data[indextr].UserName}}
                                     </vs-td>
                                     <vs-td :data="data[indextr].UserName">
                                         {{data[indextr].UserName}}
@@ -60,15 +76,28 @@
                                     <vs-td :data="data[indextr].Email">
                                         {{data[indextr].Email}}
                                     </vs-td>
-                                    <vs-td :data="data[indextr].Address">
+                                    <!-- <vs-td :data="data[indextr].Address">
                                         {{data[indextr].Address}}
-                                    </vs-td>
+                                    </vs-td> -->
                                     <vs-td :data="data[indextr].Phone">
                                         {{data[indextr].Phone}}
                                     </vs-td>
-                                    <vs-td :data="data[indextr].License">
+                                    <!-- <vs-td :data="data[indextr].License">
                                         {{data[indextr].License}}
+                                    </vs-td> -->
+                                    <vs-td :data="data[indextr].date">
+                                        <div class="centerx">
+                                            <vs-tooltip text="แก้ไขข้อมูล">
+                                                <vs-button color="rgba(112, 128, 144, 0.25)" 
+                                                type="filled" icon="drive_file_rename_outline" @click="openConfirm()"></vs-button>
+                                            </vs-tooltip>
+                                            <vs-tooltip text="ลบข้อมูล">
+                                                <vs-button color="rgba(112, 128, 144, 0.25)" 
+                                                type="filled" icon="delete" @click="openConfirm()"></vs-button>
+                                            </vs-tooltip>
+                                        </div>
                                     </vs-td>
+                                    
                                 </vs-tr>
                             </template>
                         </vs-table>
